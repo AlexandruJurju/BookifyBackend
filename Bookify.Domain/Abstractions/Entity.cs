60b5c28d47fa2fca8ec1fa275707d9b6947c724e
Alex <alexandru.i.jurju@gmail.com>
2025-03-29 17:44:56 +0200

    added apartment entity

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; init; }
+
+
+    protected Entity()
+    {
+        
+    }
+}
