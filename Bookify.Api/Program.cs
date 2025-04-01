cc0680cfed2cd0e0dd89102dc32234086c461ea1
Alex <alexandru.i.jurju@gmail.com>
2025-04-01 20:42:30 +0300

    starting presentation project

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,22 +1,27 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
